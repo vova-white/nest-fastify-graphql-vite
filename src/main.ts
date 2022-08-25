b22c7a9709f0d22076dfba79d14dde06c92d9fc1
Vladimir Belyj <belyj.vladimir@gmail.com>
2022-08-25 23:52:09 +0200

    build: replace custom vite nestjs plugin with vavite

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,54 @@
-import { bootstrap, start } from './main.bootstrap';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AddressInfo } from 'net';
+import httpDevServer from 'vavite/http-dev-server';
+import viteDevServer from 'vavite/vite-dev-server';
+import { appConfig } from './app.config';
+import { AppModule } from './app.module';
 
-(async function () {
-  const app = await bootstrap();
-  await start(app);
-})();
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter({
+      serverFactory: httpDevServer
+        ? (handler) => {
+            httpDevServer!.on('request', handler);
+            return httpDevServer!;
+          }
+        : undefined,
+    }),
+  );
+
+  app.setGlobalPrefix('api/v1');
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Nest Fastify Vite')
+    .setDescription('Awesome API 🔥')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  try {
+    const port = httpDevServer
+      ? (httpDevServer.address() as AddressInfo).port
+      : appConfig.APP_PORT;
+    await app.listen(port, appConfig.APP_HOST);
+    const url = await app.getUrl();
+
+    Logger.log(`OpenAPI on: ${url}/api`, 'Bootstrap');
+    Logger.log(`GraphQL playground on: ${url}/graphql`, 'Bootstrap');
+  } catch (err) {
+    if (err instanceof Error) viteDevServer?.ssrFixStacktrace(err);
+    viteDevServer?.config.logger.error(err);
+  }
+}
+
+bootstrap();
--- vite.config.ts
+++ vite.config.ts
@@ -1,68 +1,74 @@
 /// <reference types="vitest" />
-import multiInput from 'rollup-plugin-multi-input';
+// import multiInput from 'rollup-plugin-multi-input';
 import swc from 'unplugin-swc';
 import { defineConfig } from 'vite';
-import ViteRestart from 'vite-plugin-restart';
-import ViteNest from './vite-plugin-nestjs';
+// import ViteRestart from 'vite-plugin-restart';
+// import ViteNest from './vite-plugin-nestjs';
+import vavite from 'vavite';
 
 export default defineConfig(({ command }) => {
-  const isCjs = process.argv.some((val) => val === '--cjs');
-  const isBuild = command === 'build';
-  const appPath = './src/main.bootstrap.ts';
-  const buildInput = ['./src/**/*.ts']; // './src/main.ts for bundle to one file
+  // const isCjs = process.argv.some((val) => val === '--cjs');
+  // const isBuild = command === 'build';
+  const appPath = './src/main.ts';
+  // const buildInput = ['./src/**/*.ts', '!**/*spec.ts']; // './src/main.ts for bundle to one file
 
   return {
-    build: {
-      ssr: true,
-      rollupOptions: {
-        input: isBuild ? buildInput : appPath,
-        output: {
-          format: isBuild && isCjs ? 'cjs' : 'es',
-          sourcemap: true,
-        },
-        plugins: [isBuild && multiInput()],
-      },
-    },
-    test: {
-      include: ['**/*.{spec,test,e2e-spec}.{js,mjs,cjs,ts}'],
-    },
-    optimizeDeps: {
-      // To prevent error:
-      // Big integer literals are not available in the configured target environment.
-      esbuildOptions: { target: 'es2020' },
-
-      // To prevent error:
-      // Could not resolve "PACKAGE_NAME".
-      // Vite does not work well with optionnal dependencies.
-      // You can mark the path "PACKAGE_NAME" as external to exclude it from the bundle, which will remove
-      // this error. You can also surround this "require" call with a try/catch block to handle this
-      // failure at run-time instead of bundle-time.
-      exclude: [
-        '@nestjs/platform-express',
-        '@nestjs/microservices',
-        '@nestjs/websockets',
-        'cache-manager',
-        // 'class-transformer',
-        // 'class-validator',
-        'class-transformer/storage',
-        '@fastify/view',
-        // GraphQL dependencies.
-        '@apollo/subgraph',
-        'ts-morph',
-        'apollo-server-express',
-        '@apollo/gateway',
-        'fsevents',
-        'point-of-view',
-      ],
-    },
     plugins: [
       swc.vite({
         tsconfigFile: './tsconfig.build.json',
       }),
-      ViteRestart({
-        restart: ['vite.server.mjs'],
+      // ViteRestart({
+      //   restart: ['vite.server.mjs'],
+      // }),
+      vavite({
+        serverEntry: appPath,
+        reloadOn: 'static-deps-change',
+        serveClientAssetsInDev: true,
       }),
-      ViteNest({ appPath }),
+      //ViteNest({ appPath }),
     ],
+    // build: {
+    //   ssr: true,
+    //   rollupOptions: {
+    //     input: isBuild ? buildInput : appPath,
+    //     output: {
+    //       format: isBuild && isCjs ? 'cjs' : 'es',
+    //       sourcemap: true,
+    //     },
+    //     plugins: [isBuild && multiInput()],
+    //   },
+    // },
+    // optimizeDeps: {
+    //   // To prevent error:
+    //   // Big integer literals are not available in the configured target environment.
+    //   // esbuildOptions: { target: 'es2020' },
+
+    //   // To prevent error:
+    //   // Could not resolve "PACKAGE_NAME".
+    //   // Vite does not work well with optionnal dependencies.
+    //   // You can mark the path "PACKAGE_NAME" as external to exclude it from the bundle, which will remove
+    //   // this error. You can also surround this "require" call with a try/catch block to handle this
+    //   // failure at run-time instead of bundle-time.
+    //   exclude: [
+    //     '@nestjs/platform-express',
+    //     '@nestjs/microservices',
+    //     '@nestjs/websockets',
+    //     'cache-manager',
+    //     // 'class-transformer',
+    //     // 'class-validator',
+    //     'class-transformer/storage',
+    //     '@fastify/view',
+    //     // GraphQL dependencies.
+    //     '@apollo/subgraph',
+    //     'ts-morph',
+    //     'apollo-server-express',
+    //     '@apollo/gateway',
+    //     'fsevents',
+    //     'point-of-view',
+    //   ],
+    // },
+    test: {
+      include: ['**/*.{spec,test,e2e-spec}.{js,mjs,cjs,ts}'],
+    },
   };
 });
